cebafc3e781d6d9677755dbb6e030890fef2e4a1
xsabiubiu-cmyk <xsabiubiu@gmail.com>
2025-11-21 09:37:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ with tab2:
         "Very satisfied with the product quality.",
         "Quick delivery and excellent support."
     ]
-    for feedback in customer_feedback:
-        st.write(f"- {feedback}")
+    for i,feedback in enumerate(customer_feedback):
+        st.write(f"{i+1}  {feedback}")
 with tab3:
     st.write("Content for Market Trends")
     market_trends = {
